0b243cd8fd4044e7f61c7420587c248b1448c860
Marcos Rodriguez <rodriguezpalermo.marcos@gmail.com>
2024-11-04 21:23:49 -0300

    Se agrega la clase 2 y 3 de VueJs. Portfolio Personal

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+const nombre= 'Marcos Rodriguez - Desarrollador Salesforce y Mulesoft';
+
+</script>
+
+<template>
+<header>
+  <h1>{{ nombre }}</h1>
+  <p>Bienvenido a Vue 3</p>
+</header>
+
+</template>
+
+<style scoped>
+
+</style>
